ca25171f46277f8f9f22402667318585756abdff
badrudtamam035 <107450112+badrudtamam035@users.noreply.github.com>
2022-06-15 05:29:34 +0700

    Add files via upload

---
+++ tugas uts pak rofi.py
@@ -0,0 +1,56 @@
+tugas uts. py
+public class mahasiswa{
+
+private staring nama;
+
+private int umur;
+
+private double tinggi;
+
+
+public Mahasiswa(Staring nama,int umur,double tinggi){
+
+this nama=nm;
+
+this umur=umr;
+
+this tinggi=tg;
+
+}
+
+public void getNama(){
+
+        return  this nama;
+
+}    
+
+ public int getUmur(){
+
+        return this umur;
+
+}     
+
+public double getTinggi(){
+
+        return this tinggi;
+
+{
+
+class TI-D {
+
+    public static void main (Staring[] args){
+
+    Mahasiswa badrud tamam=new Mahasiswa("badrud tamam",09,03,200);    []
+
+    system out println("Nama:"+badrud tamam getNama());
+
+    system out println("Umur:"+badrud tamam getUmur());
+
+    system out printl("Tinggi:"+badrud tamam getTinggi());
+
+    }
+
+}
+
+}
+
