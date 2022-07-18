337ecb6477eeac97b56a5ab5625a80491da0fdbc
badrudtamam035 <107450112+badrudtamam035@users.noreply.github.com>
2022-07-19 05:28:25 +0700

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,55 @@
+print ("GabutCrypto\n")
+
+bertanya = input ("mau beli apa..?")
+
+if bertanya=="rokok":
+    print ("\ndaftar rokok yang ada\n")
+    print ("1. Sampoerna")
+    print ("2. Surya")
+    print ("3. evo")
+
+bertanya1 = input ("mau beli rokok apa..?")
+
+if bertanya1=="Sampoerna":
+    print ("\nharganya cuma 27.000\n")
+    print ("Dana")
+    print ("Ovo\n")
+    
+elif bertanya1=="sampoerna":
+    print ("\nharganya cuma 27.000\n")
+    print ("Dana")
+    print ("Ovo\n")
+    
+elif bertanya1=="Surya":
+    print ("\nharganya cuma 25.000\n")
+    print ("Dana")
+    print ("Ovo\n")
+    
+elif bertanya1=="surya":
+    print ("\nharganya cuma 25.000\n")
+    print ("Dana")
+    print ("Ovo\n")
+    
+elif bertanya1=="Evo":
+    print ("\nharganya cuma 18.000\n")
+    print ("Dana")
+    print ("Ovo\n")
+    
+elif bertanya1=="evo":
+    print ("\nharganya cuma 18.000\n")
+    print ("Dana")
+    print ("Ovo\n")
+    
+bertanya2 = input ("pilih metode pembayaran..?")
+
+if bertanya2=="dana":
+    print ("\nsilahkan lengkapi")
+    
+elif bertanya2=="ovo":
+    print ("\nsilahkan lengkapi")
+
+print = input ("\nnomor..?")
+print = input ("pin...?")
+
+
+ 
